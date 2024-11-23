4415ce207802bb66f68250f6e498971be24ef7cd
Christopher Hampel <christopher.hampel@yaho.de>
2024-11-23 21:03:18 +0100

    edit contact

--- app/main-content/contact-me/contact-me.component.ts
+++ app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, Form, Validators, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [ FormsModule, NgClass, NgStyle ],
+  imports: [ FormsModule, NgClass, NgStyle, CommonModule, ReactiveFormsModule], 
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
@@ -15,53 +16,85 @@ export class ContactMeComponent {
   http = inject(HttpClient);
   privacyPolice = false;
 
-  checkPrivacyPolice() {
-    if(!this.privacyPolice) {
-      this.privacyPolice = true;
-    } else {
-      this.privacyPolice = false;
-    }
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      privacyPolicy: [false, Validators.requiredTrue]
+    });
   }
 
-  contactData = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  get name() {
+    return this.contactForm.get('name')!;
+  }
 
-  // onSubmit(contactForm: NgForm) {
-  //   console.log(contactForm);
-  // }
+  get email() {
+    return this.contactForm.get('email')!;
+  }
 
-  mailTest = true;
+  get message() {
+    return this.contactForm.get('message')!;
+  }
 
-  post = {
-    endPoint: 'https://deineDomain.de/sendMail.php',
-    body: (payload: any) => JSON.stringify(payload),
-    options: {
-      headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
-      },
-    },
-  };
+  get privacyPolicy() {
+    return this.contactForm.get('privacyPolicy')!;
+  }
 
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
-        .subscribe({
-          next: (response) => {
-
-            ngForm.resetForm();
-          },
-          error: (error) => {
-            console.error(error);
-          },
-          complete: () => console.info('send post complete'),
-        });
-    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
-      ngForm.resetForm();
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   mailTest = true;
+
+//   post = {
+//     endPoint: 'https://deineDomain.de/sendMail.php',
+//     body: (payload: any) => JSON.stringify(payload),
+//     options: {
+//       headers: {
+//         'Content-Type': 'text/plain',
+//         responseType: 'text',
+//       },
+//     },
+//   };
+
+//   onSubmit(ngForm: NgForm) {
+//     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+//       this.http.post(this.post.endPoint, this.post.body(this.contactData))
+//         .subscribe({
+//           next: (response) => {
+
+//             ngForm.resetForm();
+//           },
+//           error: (error) => {
+//             console.error(error);
+//           },
+//           complete: () => console.info('send post complete'),
+//         });
+//     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+//       ngForm.resetForm();
+//     }
+//   }
+// }
